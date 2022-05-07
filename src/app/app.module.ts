9c58f74fe8f1e54263bbbc496d5bfa510d32cb93
Lucas Aster <asterlc102@gmail.com>
2022-05-07 20:33:53 -0300

    add locales pt,  display data in table,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +11,8 @@ import { FormComponent } from './components/form-component/form.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 //Modules
 import { AppRoutingModule } from './app.routes';
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +26,9 @@ import { AppRoutingModule } from './app.routes';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
