b31aed6340b025cc5af529191abc506547da16cd
Lucas Aster <asterlc102@gmail.com>
2022-05-07 13:09:39 -0300

    fix /home found file index.html

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 6060;
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', (req, res, next) => {
-    res.sendFile(__dirname + 'dist/frontend/index.html');
+app.get('/home', function (req, res) {
+    res.sendFile(path.join(path.resolve('dist/frontend'), 'index.html'));
 });
 
 app.listen(PORT, () => {
